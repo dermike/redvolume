01e1f7199583562a0a9736645732ab8f9ad160b9
Mikael Jergefelt <mike@redvolume.com>
2014-03-31 19:07:12 +0200

    Bootstrap updated to 3.1.1 and Gulp tasks to minify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
-var uncss = require('gulp-uncss');
+var uncss = require('gulp-uncss'),
+    concatCss = require('gulp-concat-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifycss = require('gulp-minify-css');
+
 gulp.task('default', function() {
-    gulp.src('css/bootstrap.css')
-        .pipe(uncss({
-            html: ['index.html', 'about.html', 'allnews.html', 'releases.html'],
-            ignore: ['.collapsing']
-        }))
-        .pipe(gulp.dest('./out'));
+  gulp.src(['css/bootstrap.css', 'css/custom.css'])
+    .pipe(uncss({
+      html: ['index.html', 'about.html', 'allnews.html', 'releases.html'],
+      ignore: ['.collapsing', '.collapse.in']
+    }))
+    .pipe(concatCss("bundle.css"))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./out'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(['js/jquery-2.1.0.js', 'js/bootstrap.js'])
+    .pipe(uglify())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./out'))
 });
