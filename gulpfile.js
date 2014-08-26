7759d3f4a6aa8438c8a76e044bf2a437a3e79a18
Mikael Jergefelt <mike@redvolume.com>
2014-08-26 20:27:32 +0200

    S3 upload gulp task added - 'gulp s3' for all files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var uncss = require('gulp-uncss'),
     concatCss = require('gulp-concat-css'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    minifycss = require('gulp-minify-css');
+    minifycss = require('gulp-minify-css'),
+    s3 = require("gulp-s3"),
+    fs = require("fs"),
+    aws = JSON.parse(fs.readFileSync('aws.json'));
 
 gulp.task('css', function() {
   gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css', 'css/custom.css'])
@@ -25,4 +28,26 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./js'));
 });
 
+gulp.task('s3root', function() {
+  gulp.src(['index.html', 'about.html', 'allnews.html', 'releases.html', 'rss.xml'])
+    .pipe(s3(aws));
+});
+
+gulp.task('s3css', function() {
+  gulp.src(['css/bundle.css'])
+    .pipe(s3(aws, {uploadPath: 'css/'}));
+});
+
+gulp.task('s3js', function() {
+  gulp.src(['css/all.js'])
+    .pipe(s3(aws, {uploadPath: 'js/'}));
+});
+
+gulp.task('s3img', function() {
+  gulp.src(['img/**'])
+    .pipe(s3(aws, {uploadPath: 'img/'}));
+});
+
 gulp.task('default', ['css', 'scripts']);
+
+gulp.task('s3', ['s3root', 's3css', 's3js', 's3img'])
